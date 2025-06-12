c63a186dcbf4986cb7dd567690d9f100dfd76239
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 11:25:11 +0200

    Create dim_user.sql

---
+++ Models/dim_user.sql
@@ -0,0 +1,16 @@
+WITH source AS (
+    SELECT DISTINCT
+        age,
+        gender
+    FROM {{ source('dbt_demo', 'productivity') }}
+),
+
+with_ids AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['age', 'gender']) }} AS user_id,
+        age,
+        gender
+    FROM source
+)
+
+SELECT * FROM with_ids;
