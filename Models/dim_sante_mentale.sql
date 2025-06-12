c44384986f79fda1d8c739d91c4c12fcaf89e664
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 11:59:50 +0200

    Update dim_sante_mentale.sql 2

--- Models/dim_sante_mentale.sql
+++ Models/dim_sante_mentale.sql
@@ -1,25 +1,19 @@
-WITH source AS (
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'stress_level',
+        'days_feeling_burnout_per_month',
+        'weekly_offline_hours',
+        'job_satisfaction_score'
+    ]) }} AS sante_id,
+    stress_level,
+    days_feeling_burnout_per_month,
+    weekly_offline_hours,
+    job_satisfaction_score
+FROM (
     SELECT DISTINCT
         stress_level,
         days_feeling_burnout_per_month,
         weekly_offline_hours,
         job_satisfaction_score
     FROM {{ source('dbt_productivity', 'productivity') }}
-),
-
-with_ids AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key([
-            'stress_level',
-            'days_feeling_burnout_per_month',
-            'weekly_offline_hours',
-            'job_satisfaction_score'
-        ]) }} AS sante_id,
-        stress_level,
-        days_feeling_burnout_per_month,
-        weekly_offline_hours,
-        job_satisfaction_score
-    FROM source
-)
-
-SELECT * FROM with_ids;
+) AS distinct_values
