d95fccf75bcc1623104b91c7f2c53675a6759c31
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 12:02:23 +0200

    Update dim_productivite.sql 2

--- Models/dim_productivite.sql
+++ Models/dim_productivite.sql
@@ -1,16 +1,13 @@
-WITH source AS (
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'perceived_productivity_score',
+        'actual_productivity_score'
+    ]) }} AS productivite_id,
+    perceived_productivity_score,
+    actual_productivity_score
+FROM (
     SELECT DISTINCT
         perceived_productivity_score,
         actual_productivity_score
     FROM {{ source('dbt_productivity', 'productivity') }}
-),
-
-with_ids AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key(['perceived_productivity_score', 'actual_productivity_score']) }} AS productivite_id,
-        perceived_productivity_score,
-        actual_productivity_score
-    FROM source
-)
-
-SELECT * FROM with_ids;
+) AS distinct_values
