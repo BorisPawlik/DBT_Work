c32574a26fd7cdb380b309e9ecf547b4ba78993d
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 11:29:02 +0200

    Create dim_productivite.sql

---
+++ Models/dim_productivite.sql
@@ -0,0 +1,16 @@
+WITH source AS (
+    SELECT DISTINCT
+        perceived_productivity_score,
+        actual_productivity_score
+    FROM {{ source('dbt_demo', 'productivity') }}
+),
+
+with_ids AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['perceived_productivity_score', 'actual_productivity_score']) }} AS productivite_id,
+        perceived_productivity_score,
+        actual_productivity_score
+    FROM source
+)
+
+SELECT * FROM with_ids;
