4e457bd55cf17ba0374d2bfc29de83d22ad5ed1d
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 12:02:53 +0200

    Update dim_platform.sql 2

--- Models/dim_platform.sql
+++ Models/dim_platform.sql
@@ -1,4 +1,17 @@
-WITH source AS (
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'social_platform_preference',
+        'daily_social_media_time',
+        'number_of_notifications',
+        'uses_focus_apps',
+        'has_digital_wellbeing_enabled'
+    ]) }} AS platform_id,
+    social_platform_preference,
+    daily_social_media_time,
+    number_of_notifications,
+    uses_focus_apps,
+    has_digital_wellbeing_enabled
+FROM (
     SELECT DISTINCT
         social_platform_preference,
         daily_social_media_time,
@@ -6,23 +19,4 @@ WITH source AS (
         uses_focus_apps,
         has_digital_wellbeing_enabled
     FROM {{ source('dbt_productivity', 'productivity') }}
-),
-
-with_ids AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key([
-            'social_platform_preference',
-            'daily_social_media_time',
-            'number_of_notifications',
-            'uses_focus_apps',
-            'has_digital_wellbeing_enabled'
-        ]) }} AS platform_id,
-        social_platform_preference,
-        daily_social_media_time,
-        number_of_notifications,
-        uses_focus_apps,
-        has_digital_wellbeing_enabled
-    FROM source
-)
-
-SELECT * FROM with_ids;
+) AS distinct_values
