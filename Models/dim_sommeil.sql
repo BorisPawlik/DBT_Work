20ea1aa427b59668a20c2f1316829670f6e0caf7
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 12:03:44 +0200

    Update dim_sommeil.sql 2

--- Models/dim_sommeil.sql
+++ Models/dim_sommeil.sql
@@ -1,18 +1,16 @@
-WITH source AS (
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'sleep_hours',
+        'screen_time_before_sleep',
+        'coffee_consumption_per_day'
+    ]) }} AS sommeil_id,
+    sleep_hours,
+    screen_time_before_sleep,
+    coffee_consumption_per_day
+FROM (
     SELECT DISTINCT
         sleep_hours,
         screen_time_before_sleep,
         coffee_consumption_per_day
     FROM {{ source('dbt_productivity', 'productivity') }}
-),
-
-with_ids AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key(['sleep_hours', 'screen_time_before_sleep', 'coffee_consumption_per_day']) }} AS sommeil_id,
-        sleep_hours,
-        screen_time_before_sleep,
-        coffee_consumption_per_day
-    FROM source
-)
-
-SELECT * FROM with_ids;
+) AS distinct_values
