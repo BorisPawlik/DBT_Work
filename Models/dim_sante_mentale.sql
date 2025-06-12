fe50adb3795105e2a7e067f4feebaf3b77d738e2
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-12 11:29:38 +0200

    Create dim_sante_mentale.sql

---
+++ Models/dim_sante_mentale.sql
@@ -0,0 +1,25 @@
+WITH source AS (
+    SELECT DISTINCT
+        stress_level,
+        days_feeling_burnout_per_month,
+        weekly_offline_hours,
+        job_satisfaction_score
+    FROM {{ source('dbt_demo', 'productivity') }}
+),
+
+with_ids AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+            'stress_level',
+            'days_feeling_burnout_per_month',
+            'weekly_offline_hours',
+            'job_satisfaction_score'
+        ]) }} AS sante_id,
+        stress_level,
+        days_feeling_burnout_per_month,
+        weekly_offline_hours,
+        job_satisfaction_score
+    FROM source
+)
+
+SELECT * FROM with_ids;
