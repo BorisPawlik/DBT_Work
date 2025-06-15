0ed5b47cd5ec8943bf7a4460c4460c9a5519feb6
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-16 01:01:38 +0200

    Update and rename dim_user.sql to dim_user2.sql

---
+++ Models/bien_etre_project/dim_user2.sql
@@ -0,0 +1,14 @@
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['age', 'gender', 'uses_focus_apps', 'has_digital_wellbeing_enabled']) }} AS user_id,
+    age,
+    gender,
+    uses_focus_apps,
+    has_digital_wellbeing_enabled
+FROM (
+    SELECT DISTINCT
+        age,
+        gender,
+        uses_focus_apps,
+        has_digital_wellbeing_enabled
+    FROM {{ source('dbt_productivity', 'productivity') }}
+) AS distinct_values
