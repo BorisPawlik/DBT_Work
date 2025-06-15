a879cf4fca2063376f24de332ea940b2cec18659
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-16 00:59:42 +0200

    Update and rename dim_platform.sql to dim_platform2.sql

---
+++ Models/bien_etre_project/dim_platform2.sql
@@ -0,0 +1,10 @@
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'social_platform_preference',
+    ]) }} AS platform_id,
+    social_platform_preference,
+FROM (
+    SELECT DISTINCT
+        social_platform_preference,
+    FROM {{ source('dbt_productivity', 'productivity') }}
+) AS distinct_values
