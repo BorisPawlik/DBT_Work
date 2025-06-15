43dd6e7b561e2e2be1d4bef47463814efcea98fc
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-16 01:11:47 +0200

    Update dim_job2.sql

--- Models/bien_etre_project/dim_job2.sql
+++ Models/bien_etre_project/dim_job2.sql
@@ -2,9 +2,9 @@ SELECT
     {{ dbt_utils.generate_surrogate_key([
         'job_type'
     ]) }} AS job_id,
-    job_type,
+    job_type
 FROM (
     SELECT DISTINCT
-        job_type,
+        job_type
     FROM {{ source('dbt_productivity', 'productivity') }}
 ) AS distinct_values
