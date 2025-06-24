4faf65e2cfd07a2c7c5d5ccb041454cdc32fc655
root <root@0527-095257-df4p5xpi-10-214-225-122>
2025-06-24 07:56:31 +0000

    Maj schema.yml + fichiers SQL avec table: productivity1

--- Models/bien_etre_project/dim_platform.sql
+++ Models/bien_etre_project/dim_platform.sql
@@ -6,5 +6,5 @@ SELECT
 FROM (
     SELECT DISTINCT
         social_platform_preference
-    FROM {{ source('pipeline', 'productivity2') }}
+    FROM {{ source('pipeline', 'productivity1') }}
 ) AS distinct_values
