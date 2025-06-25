abf784ab711e11465b5403db41d00ade7cfade45
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-25 15:29:05 +0200

    Update dim_job.sql

--- Models/bien_etre_project/dim_job.sql
+++ Models/bien_etre_project/dim_job.sql
@@ -1,14 +1,16 @@
 {{ config(
-    materialized='table'
+    materialized='incremental',
+    unique_key='job_id'
 ) }}
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key([
-        'job_type'
-    ]) }} AS job_id,
+    {{ dbt_utils.generate_surrogate_key(['job_type']) }} AS job_id,
     job_type
 FROM (
     SELECT DISTINCT
         job_type
     FROM {{ source('productivity', 'productivity') }}
+    {% if is_incremental() %}
+        WHERE _modified > (SELECT MAX(_modified) FROM {{ this }})
+    {% endif %}
 ) AS distinct_values
