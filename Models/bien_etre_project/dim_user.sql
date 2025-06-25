efdc7461901432803c9fa4f6854bced954030321
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-25 16:08:20 +0200

    Update dim_user.sql

--- Models/bien_etre_project/dim_user.sql
+++ Models/bien_etre_project/dim_user.sql
@@ -1,24 +1,19 @@
-{{ config(materialized='incremental', unique_key='user_id') }}
+{{ config(
+    materialized='table',
+    unique_user_key='user_id'
+) }}
 
--- Récupération de la date de dernière mise à jour de la table cible (si elle existe)
-{% if is_incremental() %}
-    with latest_modified as (
-        select max(_modified) as max_modified
-        from {{ this }}
-    )
-{% endif %}
-
--- Vue temporaire avec les données distinctes à insérer
-, distinct_values as (
-    select distinct
-        user_name
-    from {{ source('productivity', 'productivity') }}
-    {% if is_incremental() %}
-        where _modified > (select max_modified from latest_modified)
-    {% endif %}
-)
-
-select
-    md5(cast(coalesce(cast(user_name as string), '_dbt_utils_surrogate_key_null_') as string)) as user_id,
-    user_name
-from distinct_values
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['age', 'gender', 'uses_focus_apps', 'has_digital_wellbeing_enabled']) }} AS user_id,
+    age,
+    gender,
+    uses_focus_apps,
+    has_digital_wellbeing_enabled
+FROM (
+    SELECT DISTINCT
+        age,
+        gender,
+        uses_focus_apps,
+        has_digital_wellbeing_enabled
+    FROM {{ source('productivity', 'productivity') }}
+) AS distinct_values
