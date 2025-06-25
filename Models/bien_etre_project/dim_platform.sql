be1c979519677a598c6001a9925f37cc11b0ec72
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-25 16:06:51 +0200

    Update dim_platform.sql

--- Models/bien_etre_project/dim_platform.sql
+++ Models/bien_etre_project/dim_platform.sql
@@ -1,25 +1,15 @@
-{{ config(materialized='incremental', unique_key='platform_id') }}
+{{ config(
+    materialized='incremental',
+    uni
+) }}
 
--- Récupération de la date de dernière modification si en mode incrémental
-{% if is_incremental() %}
-    with latest_modified as (
-        select max(_modified) as max_modified
-        from {{ this }}
-    )
-{% endif %}
-
--- Vue temporaire des plateformes distinctes
-, distinct_values as (
-    select distinct
-        platform
-    from {{ source('productivity', 'productivity') }}
-    {% if is_incremental() %}
-        where _modified > (select max_modified from latest_modified)
-    {% endif %}
-)
-
--- Final select avec génération d'un identifiant unique
-select
-    md5(cast(coalesce(cast(platform as string), '_dbt_utils_surrogate_key_null_') as string)) as platform_id,
-    platform
-from distinct_values
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'social_platform_preference'
+    ]) }} AS platform_id,
+    social_platform_preference
+FROM (
+    SELECT DISTINCT
+        social_platform_preference
+    FROM {{ source('productivity', 'productivity') }}
+) AS distinct_values
