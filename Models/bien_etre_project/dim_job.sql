c463f8f37b6e5ad5cc2c3228db3654bd89964e74
Boris PAWLIK <156550341+BorisPawlik@users.noreply.github.com>
2025-06-25 16:04:48 +0200

    Update dim_job.sql

--- Models/bien_etre_project/dim_job.sql
+++ Models/bien_etre_project/dim_job.sql
@@ -1,25 +1,14 @@
-{{ config(materialized='incremental', unique_key='job_id') }}
+{{ config(
+    materialized='incremental'
+) }}
 
--- CTE pour récupérer la dernière date de modification (en mode incrémental)
-{% if is_incremental() %}
-    with latest_modified as (
-        select max(_modified) as max_modified
-        from {{ this }}
-    )
-{% endif %}
-
--- CTE pour extraire les valeurs distinctes
-, distinct_values as (
-    select distinct
-        job
-    from {{ source('productivity', 'productivity') }}
-    {% if is_incremental() %}
-        where _modified > (select max_modified from latest_modified)
-    {% endif %}
-)
-
--- Résultat final avec génération d'un identifiant unique
-select
-    md5(cast(coalesce(cast(job as string), '_dbt_utils_surrogate_key_null_') as string)) as job_id,
-    job
-from distinct_values
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'job_type'Add commentMore actions
+    ]) }} AS job_id,
+    job_type
+FROM (
+    SELECT DISTINCT
+        job_type
+    FROM {{ source('productivity', 'productivity') }}
+) AS distinct_values
